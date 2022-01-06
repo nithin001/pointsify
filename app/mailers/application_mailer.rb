2846c15c62954a8399834b9b055f97dfa38e2be6
Nithin Gunasekaran <nithin.ktg@gmail.com>
2022-01-06 19:08:00 +0530

    Fix mailer default from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'support@em9250.pointsify.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     :password => ENV["SENDGRID_API_KEY"], # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'pointsify.com',
+    :domain => 'em9250.pointsify.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
