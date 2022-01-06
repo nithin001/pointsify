daa047f683952e0b960eac9c33fc9ab405e552fa
Nithin Gunasekaran <nithin.ktg@gmail.com>
2022-01-06 09:04:47 +0530

    Rearrange migration scripts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_041109) do
+ActiveRecord::Schema.define(version: 2022_01_05_041110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,10 +37,10 @@ ActiveRecord::Schema.define(version: 2022_01_05_041109) do
     t.string "phone", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "remember_created_at"
-    t.integer "failed_attempts", default: 0, null: false
-    t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "failed_attempts", default: 0, null: false
+    t.datetime "locked_at"
     t.index ["phone"], name: "index_customers_on_phone", unique: true
   end
 
