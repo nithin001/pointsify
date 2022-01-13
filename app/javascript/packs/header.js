b84e92f977d029d97571779c2f9aa24b990a6e7f
Nithin Gunasekaran <nithin.ktg@gmail.com>
2022-01-13 18:17:30 +0530

    Clean up landing page

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -1,45 +1,98 @@
 /* eslint-env browser */
 
-function headerLoad() {
-  $(".notice").delay(5000).fadeOut(0);
+function recolorNav() {
+  var scrollpos = window.scrollY;
+  var header = document.getElementById("header");
+  var navcontent = document.getElementById("nav-content");
+  var navactionList = document.querySelectorAll(".action-link");
+  var brandname = document.getElementById("brandname");
+  var toToggle = document.querySelectorAll(".toggleColour");
+  var logoImg = document.getElementById("logo");
+  var logoDarkImg = document.getElementById("logo-dark");
+  scrollpos = window.scrollY;
 
-  const userMenu = $('#user-menu');
-  const mobileMenu = $('#mobile-menu');
-  userMenu.addClass('hidden');
-  mobileMenu.addClass('hidden');
-  $('#user-menu-button').click(() => {
-    const isHidden = userMenu.is('.hidden');
-    if (isHidden) {
-      userMenu.removeClass('hidden');
-    } else {
-      userMenu.addClass('hidden');
+  if (scrollpos > 10) {
+    header.classList.add("bg-white");
+    logoImg.classList.add("hidden");
+    logoDarkImg.classList.remove("hidden");
+    for (var i = 0; i < navactionList.length; i++) {
+      let navaction = navactionList[i]
+      navaction.classList.remove("bg-white");
+      navaction.classList.add("bg-gradient-to-r");
+      navaction.classList.add("from-pink-300");
+      navaction.classList.add("via-purple-300");
+      navaction.classList.add("to-indigo-400");
+      navaction.classList.remove("text-gray-800");
+      navaction.classList.add("text-white");
     }
-  });
-
-  $('#mobile-menu-button').click(() => {
-    const isHidden = mobileMenu.is('.hidden');
-    if (isHidden) {
-      mobileMenu.removeClass('hidden');
-    } else {
-      mobileMenu.addClass('hidden');
+    for (var i = 0; i < toToggle.length; i++) {
+      toToggle[i].classList.add("text-gray-800");
+      toToggle[i].classList.remove("text-white");
     }
-  });
-}
+    header.classList.add("shadow");
+    navcontent.classList.remove("bg-gray-100");
+    navcontent.classList.add("bg-white");
+  } else {
+    header.classList.remove("bg-white");
+    logoImg.classList.remove("hidden");
+    logoDarkImg.classList.add("hidden");
+    for (var i = 0; i < navactionList.length; i++) {
+      let navaction = navactionList[i]
+      navaction.classList.remove("bg-gradient-to-r");
+      navaction.classList.remove("from-pink-300");
+      navaction.classList.remove("via-purple-300");
+      navaction.classList.remove("to-indigo-400");
+      navaction.classList.add("bg-white");
+      navaction.classList.remove("text-white");
+      navaction.classList.add("text-gray-800");
+    }
+    //Use to switch toggleColour colours
+    for (var i = 0; i < toToggle.length; i++) {
+      toToggle[i].classList.add("text-white");
+      toToggle[i].classList.remove("text-gray-800");
+    }
 
-function resetClick(event, container) {
-  const menu = $(`#${container}`);
-  const menuButton = $(`#${container}-button`);
-  // if the target of the click isn't the container nor a descendant of the container
-  if (!menuButton.is(event.target)
-      && !menu.is(event.target)
-      && menu.has(event.target).length === 0) {
-    menu.addClass('hidden');
+    header.classList.remove("shadow");
+    navcontent.classList.remove("bg-white");
+    // navcontent.classList.add("bg-gray-100");
   }
 }
 
-$(document).mouseup((e) => {
-  resetClick(e, 'user-menu');
-  resetClick(e, 'mobile-menu');
+function checkParent(t, elm) {
+  while (t.parentNode) {
+    if (t == elm) {
+      return true;
+    }
+    t = t.parentNode;
+  }
+  return false;
+}
+
+function addLinks(){
+  var navMenuDiv = document.getElementById("nav-content");
+  var navMenu = document.getElementById("nav-toggle");
+
+  document.onclick = check;
+  function check(e) {
+    var target = (e && e.target) || (event && event.srcElement);
+
+    if (!checkParent(target, navMenuDiv)) {
+      if (checkParent(target, navMenu)) {
+        if (navMenuDiv.classList.contains("hidden")) {
+          navMenuDiv.classList.remove("hidden");
+        } else {
+          navMenuDiv.classList.add("hidden");
+        }
+      } else {
+        navMenuDiv.classList.add("hidden");
+      }
+    }
+  }
+}
+
+document.addEventListener("scroll", function () {
+  recolorNav();
 });
 
-document.addEventListener('turbolinks:load', headerLoad, false);
+document.addEventListener('turbolinks:load', addLinks, false);
+document.addEventListener('turbolinks:load', recolorNav, false);
